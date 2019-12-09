b8a673c0323fc3409235a404754220f53ea2bd4a
Bobae Kang <bkang.dev@gmail.com>
2019-12-09 17:26:40 -0600

    add a check if Arrests table already exists

--- create.go
+++ create.go
@@ -6,10 +6,21 @@ import (
 )
 
 func CreateArrestsTable(db *sql.DB) {
-	_, err := db.Exec("CREATE TABLE Arrests ( year integer, value integer )")
-	if err != nil {
-		log.Fatal(err)
-	}
+	if exist := CheckExistArrestsTable(db); !exist {
+		_, err := db.Exec("CREATE TABLE Arrests ( year integer, value integer )")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	log.Println("note: Arrests table created.")
+		log.Println("note: Arrests table created.")
+	}
+}
+
+func CheckExistArrestsTable(db *sql.DB) (exist bool) {
+	err := db.QueryRow("SELECT name FROM sqlite_master WHERE type='table' AND name='Arrests'")
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
 }
